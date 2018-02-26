c1cd6f98d6a3945ef282c180917fc93afbd94947
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-02-26 15:34:24 +0100

    fix devserver startup usind Spring Boot ServletInitializer

--- restserver/src/main/java/org/javacream/people/rest/restserver/RestserverApplication.java
+++ restserver/src/main/java/org/javacream/people/rest/restserver/RestserverApplication.java
@@ -2,9 +2,10 @@ package org.javacream.people.rest.restserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestserverApplication {
+public class RestserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestserverApplication.class, args);
